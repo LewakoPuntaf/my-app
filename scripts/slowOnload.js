ddbe7fd85d4bc5303744b74527c248d46d66b4f5
Arthur Pavlenko <owncreator@yandex.ru>
2018-10-12 21:13:43 +0700

    Main page complete

--- scripts/slowOnload.js
+++ scripts/slowOnload.js
@@ -1,9 +1,17 @@
-$(document.onload).ready(function(){
+$(document.onload).ready(function() {
 
   $(".top-menu").fadeIn(500);
 
   var bodyText = $('.body-text-item');
-  bodyText.each(function(e, v) {
-    $('.body-text-item').eq(e).slideDown(600 * e);
-  });
+
+  setTimeout(function() {
+    bodyText.each(function(e, v) {
+      $('.body-text-item').eq(e).slideDown(1000 * e, easing = 'easeInOutQuart');
+    });
+  }, 1000);
+
+  setTimeout(function() {
+    $(".start-block").fadeIn(500);
+  }, 3000);
+
 });
